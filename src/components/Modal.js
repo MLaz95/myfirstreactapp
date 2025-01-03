86be3df00f86dc2503b7740b9da274e07f831e49
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2025-01-03 15:51:40 +0100

    basic modal styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,26 +1,34 @@
+import styles from "../styles/Modal.module.css";
+
 function Modal({ newLink, isModalOpen, setModalOpen }){
 
     if(isModalOpen){
         return(
-            <form className="newLinkForm"
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    //target[0] = link-name
-                    //target[1] = link-url
-                    newLink(e.target[0].value, e.target[1].value)
-                }}
-            >
-                <div>
-                    <label htmlFor='link-name'>Name</label>
-                    <input id="link-name" type="text" />
-                </div>
-                <div>
-                    <label htmlFor='link-url'>Url</label>
-                    <input id="link-url" type="text" />   
-                </div>
-                <button type="submit">Confirm</button>
-                <button type="button" onClick={() => setModalOpen(false)}>Close</button>
-            </form>
+            <div className={styles.modal}>
+                <h3>Add a new shortcut</h3>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        //target[0] = link-name
+                        //target[1] = link-url
+                        newLink(e.target[0].value, e.target[1].value)
+                    }}
+                >
+                    <div className={styles.input}>
+                        <label htmlFor='link-name'>Name</label>
+                        <input id="link-name" type="text" />
+                    </div>
+                    <div className={styles.input}>
+                        <label htmlFor='link-url'>Url</label>
+                        <input id="link-url" type="text" />   
+                    </div>
+                    <div className={styles.buttons}>
+                        <button type="submit">Confirm</button>
+                        <button type="button" onClick={() => setModalOpen(false)}>Close</button>
+                    </div>
+                </form>
+            </div>
+
         )
     }
 }
